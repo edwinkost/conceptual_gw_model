715909bb013ff20d6fc18d00941d9dfbe287dd31
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-06 11:53:45 +0200

    in progress

---
+++ data/plot_discharge_station.R
@@ -0,0 +1,32 @@
+
+# R script
+
+# read table
+
+
+
+
+# calculate the drainage map based on pcr-globwb ldd
+cmd = 
+pcrcalc catchment_area.map = "catchmenttotal()"
+
+
+# write it in an "asc2map" format
+
+write.table(x, file = "", append = FALSE, quote = TRUE, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+
+
+# make a station map with station ids
+
+# make a station map with drainage areas
+
+
+pcrcalc  
+
+
+# 
+
+
